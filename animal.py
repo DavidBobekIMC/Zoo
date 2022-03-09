91b42e2aeccd63ac401f553d588c2fd89cb7e47c
David <Dbobek29@gmail.com>
2022-03-09 17:02:16 +0100

    master

---
+++ animal.py
@@ -0,0 +1,25 @@
+import uuid 
+import datetime 
+class Animal: 
+    def __init__ (self, species_name, common_name, age): 
+        self.animal_id = str(uuid.uuid4())
+        self.species_name = species_name 
+        self.common_name = common_name 
+        self.age = age 
+        self.feeding_record = [] 
+        self.enclosure = None 
+        self.care_taker = None 
+        self.medical_checkup = None
+        
+        # add more as required here 
+        
+    # simply store the current system time when this method is called    
+    def feed(self): 
+        self.feeding_record.append ( datetime.datetime.now()) 
+
+    def vet(self):
+        self.medical_checkup =  datetime.datetime.now()
+        
+
+
+            
