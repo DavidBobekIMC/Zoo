0c70b378e4da8d93a063bed6e00615e5d3d1160f
David <Dbobek29@gmail.com>
2022-03-30 10:46:53 +0200

    master

--- test.py
+++ test.py
@@ -9,29 +9,39 @@ from zoo import *
 from enclosure import *
 import random
 
-
+#testing if animal was added into the zoo
 def testAnimalToZoo():
     new_zoo = Zoo()
-    current_number_animals = len(my_zoo.animals)
     new_animal = Animal ("Lion", "David", 2) 
     new_zoo.addAnimal (new_animal)
-    assert len(new_zoo.animals) != current_number_animals
+    assert new_animal in new_zoo.animals
     
-    
+
+#testing if we create an animal and enclosure,the animal is capable of haivng a home and vice versa 
 def testAnimalMakeHome():
     new_animal = Animal ("Lion", "David", 2) 
     new_enclosure = Enclosure("Cage",250)
     new_animal.assign_enclosure(new_enclosure)
     assert new_animal.enclosure == new_enclosure
+    assert new_animal in new_enclosure.animals
     
-    
+
+# basic testing that record of a medical checkup is being added to the record
 def testAnimalVet():
     new_animal = Animal ("Lion", "David", 2) 
     number_of_medical_checkups = len(new_animal.medical_checkup)
     new_animal.vet()
     assert number_of_medical_checkups + 1 == len(new_animal.medical_checkup)
     
+#Similiar function as vet
+def testFeeding():
+    new_animal = Animal("Lion","David",2)
+    defaultNumFeeds = len(new_animal.feeding_record)
+    new_animal.feed()
+    assert defaultNumFeeds+1 == len(new_animal.feeding_record) 
+    
 
+#testing that if animal dies it is removed from the zoo and enclosure
 def testAnimalDie():
     new_zoo = Zoo()
     new_animal = Animal ("Lion", "David", 2)
@@ -40,12 +50,12 @@ def testAnimalDie():
     new_enclosure = Enclosure("Cage",250)
     new_zoo.addAnimal (new_animal)
     new_animal.assign_enclosure(new_enclosure)
-    new_zoo.animal_die(new_animal,new_enclosure)
-    
-    
-    
+    new_zoo.animal_die(new_animal,new_enclosure)   
     assert new_animal not in new_zoo.animals
+    assert new_animal not in new_enclosure.animals
     
+
+#testing that the Enclosure can be cleaned and the record is being added into the records
 def testCleaningEnclosure():
     new_zoo = Zoo()
     new_enclosure = Enclosure("Cage",250)
@@ -55,6 +65,7 @@ def testCleaningEnclosure():
     assert num_of_cleanups +1 == len(new_enclosure.cleaning_records)
 
 
+#testing that if an animal already has an caretaker it can not have anyother
 def testEmployeeTakesCare():
     new_caretaker1 = Caretaker("John","21/11045")
     new_caretaker2 = Caretaker("Patrick","56/78224")
@@ -63,13 +74,11 @@ def testEmployeeTakesCare():
     new_animal.assign_caretaker(new_caretaker2)
     assert new_animal.care_taker == new_caretaker1
     assert new_animal in new_caretaker1.animals
+    assert new_animal not in new_caretaker2.animals
     
-def testFeeding():
-    new_animal = Animal("Lion","David",2)
-    defaultNumFeeds = len(new_animal.feeding_record)
-    new_animal.feed()
-    assert defaultNumFeeds+1 == len(new_animal.feeding_record) 
-    
+
+#Testing that new animal is not only created bu created into the same enclosure as its mother
+
 def testBirth():
     new_animal = Animal("Lion","David",2)
     new_enclosure = Enclosure("Cage",1000)
@@ -80,7 +89,7 @@ def testBirth():
     assert new_animal.animal_id != new_child.animal_id
 
 
-#delete enclosure
+#testing that if we delete the enclosure that animals from the enclosure being transferred into a new one 
 
 def testDeleteEnclosure():
     new_zoo = Zoo()
@@ -108,8 +117,6 @@ def testDeleteEnclosure():
     assert new_enclosure not in my_zoo.all_Enclosures
     
     
-
-testDeleteEnclosure()
 
     
     
